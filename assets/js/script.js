b7146b8982fb7bdeb4d88cca3d2b46a066d85727
irfan-dahir <irfan.dahir@gmail.com>
2018-03-11 01:04:35 +0500

    polish

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ $(function() {
 	if (recent !== null && recent.length > 0) {
 		recent = recent.reverse();
 		for(k in recent) {
-			console.log(k*250);
 			$('.recent_wrap').append('<div class="recent"><label class="query">'+recent[k]+'</label><i data-it="'+k+'" class="recent_delete"><svg fill="#FFFFFF" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg></i></div>');
 		}
 	} else {
